56c7cfe762b5f54cf164ce34bd3bb78e37ef1eaa
jack <jackzhoumine@gmail.com>
2022-10-22 06:15:34 +0800

    事件

--- src/stories/ChakraButton.stories.jsx
+++ src/stories/ChakraButton.stories.jsx
@@ -2,25 +2,35 @@
  * @Description :
  * @Date        : 2022-10-22 05:06:51 +0800
  * @Author      : JackChou
- * @LastEditTime: 2022-10-22 05:52:15 +0800
+ * @LastEditTime: 2022-10-22 06:15:13 +0800
  * @LastEditors : JackChou
  */
 import React from 'react'
 import { Button } from '@chakra-ui/react'
-
+import { actions, action } from '@storybook/addon-actions'
 export default {
   title: 'Chakra/Button',
   component: Button,
   argTypes: {
     children: { control: 'text' },
     colorScheme: { control: 'color' },
-    onClick: { action: 'click' },
+    // onClick: { action: 'click' },
   },
 }
 
 const Template = args => <Button {...args} />
+
 export const ColorButton = Template.bind()
 ColorButton.args = {
   colorScheme: 'red',
   children: 'Button',
+  // NOTE 事件
+  // onClick: action('onClick'),
+  ...actions('onClick', 'onMouseOver'),
 }
+
+// export const ActionsButton = Template.bind()
+// ActionsButton.args = {
+//   colorScheme: 'red',
+//   children: 'Button',
+// }
