a2f9527f77cfe8520b85880631d0a06fad879dc0
jack <jackzhoumine@gmail.com>
2022-10-22 19:25:02 +0800

    Updates

--- assets/preview.be38fcee.js
+++
@@ -1,2 +0,0 @@
-const r={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},options:{storySort:(o,e)=>o[1].kind===e[1].kind?0:o[1].id.localeCompare(e[1].id,void 0,{numeric:!0})}};export{r as parameters};
-//# sourceMappingURL=preview.be38fcee.js.map
